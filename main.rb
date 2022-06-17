9356d18ffd48a87644d67a0ff73451b0b1360d87
Aaron Keegan <AKeegan.Dev@gmail.com>
2022-06-17 15:51:59 -0400

    buggy code

---
+++ main.rb
@@ -0,0 +1,16 @@
+#Aaron
+require 'pry'
+
+arr = [1,2,3,4,5]
+arr.each do |x|
+  binding.pry
+end
+
+# Retky
+# puts "guess number 2"
+# guess = gets.chomp
+# puts guess.equal? 2
+
+# Sediq
+# guess2 = gets.chomp
+# puts guess2.equal? 2
